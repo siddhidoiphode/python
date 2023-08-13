482743a934d3525cf2445f505d76e23f9ce846d0
siddhidoiphode <141642695+siddhidoiphode@users.noreply.github.com>
2023-08-13 10:12:38 +0530

    Create alterbatingletteradd.py
    program for alternating concadation og letters of 2 strings

---
+++ alterbatingletteradd.py
@@ -0,0 +1,12 @@
+if l1>=l2 :
+  for i in range(0,l2):
+    new=new+str1[i]+str2[i]
+else:
+  for i in range(0,l1):
+    new=new+str1[i]+str2[i]   
+print(new)
+if l1>l2 :
+  sub=l1-l2
+if l2>l1 :
+  sub=l2-l1 
+print(sub)
