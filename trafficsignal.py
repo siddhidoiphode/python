581faa98cf8c1718c53b2f08220ba502911d9578
siddhidoiphode <141642695+siddhidoiphode@users.noreply.github.com>
2023-08-13 10:16:59 +0530

    Create trafficsignal.py
    program for trafic signal

---
+++ trafficsignal.py
@@ -0,0 +1,9 @@
+c=input(" enter colour of signal : ")
+if c=="red" :
+  print("stop your car!")
+elif c=="green" :
+  print("you can go!")
+elif c=="yellow" :
+  print("you can go!")
+else :
+  print("enter valid signal colour")
