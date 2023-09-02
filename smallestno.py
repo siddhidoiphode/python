3461f19da36bddff6796c8b8b59c15e82f383ec9
siddhidoiphode <141642695+siddhidoiphode@users.noreply.github.com>
2023-09-02 23:08:44 +0530

    Create smallestno.py
    smallest numbers among 3

---
+++ smallestno.py
@@ -0,0 +1,10 @@
+a,b,c=input(" ").split()
+a=int(a)
+b=int(b)
+c=int(c)
+if(a<b and a<c):
+  print(" a is smallest")
+elif(b<a and b<c):
+  print(" b is smallest")
+if(c<a and c<b):
+  print(" c is smallest")
