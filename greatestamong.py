5fda1bfe980953655d06c80bd60fb34ebf4b91d3
siddhidoiphode <141642695+siddhidoiphode@users.noreply.github.com>
2023-09-04 22:41:42 +0530

    Create greatestamong.py
    greatest number among 3

---
+++ greatestamong.py
@@ -0,0 +1,10 @@
+a,b,c=input(" ").split()
+a=int(a)
+b=int(b)
+c=int(c)
+if(a>b and a>c):
+  print(" a is greatest")
+elif(b>a and b>c):
+  print(" b is greatest")
+if(c>a and c>b):
+  print(" c is greatest")
