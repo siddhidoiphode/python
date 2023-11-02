6460d9e617455f9baa0810f35afcf009ae099ab3
siddhidoiphode <141642695+siddhidoiphode@users.noreply.github.com>
2023-11-02 18:58:18 +0530

    Update alphabeticalrangoli.py
    alphabetical rangoli

--- alphabeticalrangoli.py
+++ alphabeticalrangoli.py
@@ -1,14 +1,19 @@
-ele = ''
-n = int(input(" enter number: "))
-store = "abcdefghijklmnopqrstuvwxyz"
-col = (2 * ((2 * n) - 1)) - 1
-for i in range(n - 1, 0, -1):
-  if i == n - 1:
-    print(store[i].center(col, '-'))
-  ele = ele + "-" + store[i]
-  ans = ele + "-" + store[i - 1] + "-"+"".join(reversed(ele))
-  ans=ans[1:-1]
-  print(ans.center(col, '-'))
-for i in range(n-1):
-  ans=ans[0:len(ans)//2]+ans[(len(ans)//2)+4:]
-  print(ans.center(col, '-'))
+def print_rangoli(n):
+    # your code goes here
+    if(n==1):
+        print("a")
+    else:
+        ele = ''
+        store = "abcdefghijklmnopqrstuvwxyz"
+        col = (2 * ((2 * n) - 1)) - 1
+        for i in range(n - 1, 0, -1):
+            if i == n - 1:
+              print(store[i].center(col, '-'))
+            ele = ele + "-" + store[i]
+            ans = ele + "-" + store[i - 1] + "-"+"".join(reversed(ele))
+            ans=ans[1:-1]
+            print(ans.center(col, '-'))
+        for i in range(n-1):
+            ans=ans[0:len(ans)//2]+ans[(len(ans)//2)+4:]
+            print(ans.center(col, '-'))
+if __name__ == '__main__':
